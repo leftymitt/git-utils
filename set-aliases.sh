7dedd9ccd2278043cf4d5859baaf54214b156f3e
pink <treefrog@riseup.net>
2015-11-17 01:46:18 +0000

    added tool to obfuscate timestamps. very obvious, so the license in this repo is probably pointless

---
+++ set-aliases.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# set aliases to add to git. they are (so far): 
+#  1. git sneaky - set time zone to UTC and make the time of day random
+
+# change to --global to use this alias in all your repos
+git config --local alias.sneaky '! RAND_OFFSET="$(shuf -i 0-86399 -n 1)" && BASE_DATE=$(date -u -d "") && RAND_DATE=$(date -u -d "$BASE_DATE +$RAND_OFFSET seconds") && export GIT_AUTHOR_DATE="$RAND_DATE" && export GIT_COMMITTER_DATE="$RAND_DATE" && git commit'
+
