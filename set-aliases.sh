236ad984295c12ed19d223c9c1f2a2cc6db5930c
pink <treefrog@riseup.net>
2015-11-24 23:32:01 +0000

    extra note on how random timestamps are generated

--- set-aliases.sh
+++ set-aliases.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
 # set aliases to add to git. they are (so far): 
-#  1. git sneaky - set time zone to UTC and make the time of day random
+#  1. git sneaky - set time zone to UTC and make the time of day random by 
+#                  taking 00:00:00 UTC of that day and adding a random offset. 
 
 # change to --global to use this alias in all your repos
-git config --local alias.sneaky '! RAND_OFFSET="$(shuf -i 0-86399 -n 1)" && BASE_DATE=$(date -u -d "") && RAND_DATE=$(date -u -d "$BASE_DATE +$RAND_OFFSET seconds") && export GIT_AUTHOR_DATE="$RAND_DATE" && export GIT_COMMITTER_DATE="$RAND_DATE" && git commit'
+git config --local alias.sneaky '! RAND_OFFSET="$(shuf -i 0-86399 -n 1)" && BASE_DATE=$(date -u -d "") && RAND_DATE=$(date -u -d "$BASE_DATE + $RAND_OFFSET seconds") && export GIT_AUTHOR_DATE="$RAND_DATE" && export GIT_COMMITTER_DATE="$RAND_DATE" && git commit'
 
