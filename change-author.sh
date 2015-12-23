c5bdb0a11ec50ef1ebc8988c31d32270a58f42e5
pink <treefrog@riseup.net>
2015-12-23 05:47:33 +0000

    update to author-changing script; remove all metadata of old authorand email address

--- change-author.sh
+++ change-author.sh
@@ -1,22 +1,30 @@
 #!/bin/sh
 
-# change the email and committer name in git history. 
+# change the email and committer name in git history. run from the base of the 
+# repository. Variables: 
 #    OLD_EMAIL = the address to be changed
 #    CORRECT_EMAIL = address to change it to
 #    CURRENT_NAME = the name of the author associalted with CORRECT_EMAIL
- 
-git filter-branch --env-filter '
+
+[ -d .git/refs/original/ ] && rm -r .git/refs/original/
+
+git filter-branch --commit-filter '
 OLD_EMAIL=""
 CORRECT_NAME=""
 CORRECT_EMAIL=""
+
 if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
-then
-export GIT_COMMITTER_NAME="$CORRECT_NAME"
-export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+	then
+	export GIT_COMMITTER_NAME="$CORRECT_NAME"
+	export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
 if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
-then
-export GIT_AUTHOR_NAME="$CORRECT_NAME"
-export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+	then
+	export GIT_AUTHOR_NAME="$CORRECT_NAME"
+	export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 fi
+
+git commit-tree "$@"
 ' --tag-name-filter cat -- --branches --tags 
+
+rm -r .git/logs/
