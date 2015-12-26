96f95d16ec2eea8d62ed709ea25b0d4bdd3fd9b2
pink <treefrog@riseup.net>
2015-12-26 14:09:07 +0000

    define a sneaky-style alias for 'git revert'

--- set-aliases.sh
+++ set-aliases.sh
@@ -6,4 +6,5 @@
 
 # change to --global to use this alias in all your repos
 git config --local alias.sneaky '! RAND_OFFSET="$(shuf -i 0-86399 -n 1)" && BASE_DATE=$(date -u -d "") && RAND_DATE=$(date -u -d "$BASE_DATE + $RAND_OFFSET seconds") && export GIT_AUTHOR_DATE="$RAND_DATE" && export GIT_COMMITTER_DATE="$RAND_DATE" && git commit'
+git config --local alias.backtrack '! RAND_OFFSET="$(shuf -i 0-86399 -n 1)" && BASE_DATE=$(date -u -d "") && RAND_DATE=$(date -u -d "$BASE_DATE + $RAND_OFFSET seconds") && export GIT_AUTHOR_DATE="$RAND_DATE" && export GIT_COMMITTER_DATE="$RAND_DATE" && git revert'
 
