71a25fb2c3a2b30cfae3c25e3963619bda0054cb
pink <treefrog@riseup.net>
2016-09-03 01:04:23 +0000

    export new names; fix typo

--- change-author.sh
+++ change-author.sh
@@ -7,23 +7,23 @@
 #    NEW_EMAIL = address to change it to
 #    NEW_AUTHOR = the name of the author associalted with NEW_EMAIL
 
-OLD_AUTHOR="$1"
-OLD_EMAIL="$2"
-NEW_AUTHOR="$3"
-NEW_EMAIL="$4"
+export OLD_AUTHOR="$1"
+export OLD_EMAIL="$2"
+export NEW_AUTHOR="$3"
+export NEW_EMAIL="$4"
 
 [ -d .git/refs/original/ ] && rm -r .git/refs/original/
 
 git filter-branch --commit-filter \ '
-   if [ ${GIT_COMMITTER_EMAIL} = ${OLD_EMAIL} ]
+   if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
    then
-      export GIT_COMMITTER_NAME=${NEW_AUTHOR}
-      export GIT_COMMITTER_EMAIL={$NEW_EMAIL}
+      export GIT_COMMITTER_NAME="$NEW_AUTHOR"
+      export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
    fi
-   if [ ${GIT_AUTHOR_EMAIL} = ${OLD_EMAIL} ]
+   if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
    then
-      export GIT_AUTHOR_NAME=${NEW_AUTHOR}
-      export GIT_AUTHOR_EMAIL=${NEW_EMAIL}
+      export GIT_AUTHOR_NAME="$NEW_AUTHOR"
+      export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
    fi
 
    git commit-tree ${@}' --tag-name-filter cat -- --branches --tags 
