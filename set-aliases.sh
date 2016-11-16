7710298a8e657fe907bfc4d33510a28275515cac
pink <treefrog@riseup.net>
2016-11-16 09:02:52 +0000

    tweak alias code for readability

--- set-aliases.sh
+++ set-aliases.sh
@@ -17,9 +17,21 @@ set -eu
 ################################################################################
 
 # change to --global to use this alias in all your repos
-git config --local alias.sneaky \
-  '! RAND_OFFSET="$(shuf -i 0-86399 -n 1)" && BASE_DATE=$(date -u -d "") && RAND_DATE=$(date -u -d "$BASE_DATE + $RAND_OFFSET seconds") && export GIT_AUTHOR_DATE="$RAND_DATE" && export GIT_COMMITTER_DATE="$RAND_DATE" && git commit'
+# git config --local alias.sneaky \
+#   '! RAND_OFFSET="$(shuf -i 0-86399 -n 1)" && BASE_DATE=$(date -u -d "") && RAND_DATE=$(date -u -d "$BASE_DATE + $RAND_OFFSET seconds") && export GIT_AUTHOR_DATE="$RAND_DATE" && export GIT_COMMITTER_DATE="$RAND_DATE" && git commit'
+# 
+# git config --local alias.backtrack \
+#   '! RAND_OFFSET="$(shuf -i 0-86399 -n 1)" && BASE_DATE=$(date -u -d "") && RAND_DATE=$(date -u -d "$BASE_DATE + $RAND_OFFSET seconds") && export GIT_AUTHOR_DATE="$RAND_DATE" && export GIT_COMMITTER_DATE="$RAND_DATE" && git revert'
 
-git config --local alias.backtrack \
-  '! RAND_OFFSET="$(shuf -i 0-86399 -n 1)" && BASE_DATE=$(date -u -d "") && RAND_DATE=$(date -u -d "$BASE_DATE + $RAND_OFFSET seconds") && export GIT_AUTHOR_DATE="$RAND_DATE" && export GIT_COMMITTER_DATE="$RAND_DATE" && git revert'
+ENV='! '
+ENV=${ENV}'RAND_OFFSET="$(shuf -i 0-86399 -n 1)" && '
+ENV=${ENV}'BASE_DATE=$(date -u -d "") && '
+ENV=${ENV}'RAND_DATE=$(date -u -d "$BASE_DATE + $RAND_OFFSET seconds") && '
+ENV=${ENV}'export GIT_AUTHOR_DATE="$RAND_DATE" && '
+ENV=${ENV}'export GIT_COMMITTER_DATE="$RAND_DATE" && '
 
+# set commit wrapper. change to --global to use this alias in all your repos.
+git config --local alias.sneaky "${ENV} git commit"
+
+# set revert wrapper. change to --global to use this alias in all your repos.
+git config --local alias.backtrack "${ENV} git revert"
